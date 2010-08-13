3bc362563adb3d247184f68fa049f7b9c819c57c
Gregor Klinke <hvellyr@users.noreply.github.com>
2010-08-13 11:38:14 +0200

    pass2: prepared for detection of test-expr-less select
    Added test to detect badly ordered select patterns.

--- bootstrap/errcodes.h
+++ bootstrap/errcodes.h
@@ -112,6 +112,7 @@ namespace heather
     E_RedefinedPattern    = 0x4402,
     E_ColonExpected       = 0x4403,
     E_ExpectedPipe        = 0x4404,
+    E_ElseNotLastPattern  = 0x4405,
 
     // module parsing
     E_MissingModName      = 0x4500,
