9a29d12dee5d471d87b39c4ca2902a842cc8de46
Gregor Klinke <hvellyr@users.noreply.github.com>
2010-09-12 21:27:41 +0200

    Use expr instead of sed

--- build/reprevision.sh
+++ build/reprevision.sh
@@ -10,4 +10,5 @@ if [ -z "$HG" ]; then
   HG=hg
 fi
 
-$HG id --debug | sed -e 's/\([a-zA-Z0-9]*\).*/\1/'
+# $HG id --debug | sed -e 's/\([a-zA-Z0-9]*\).*/\1/'
+expr "$($HG id --debug)" : '\([0-9a-zA-Z]*\)'
