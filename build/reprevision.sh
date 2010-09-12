49d52e3b051da0f5829af7d4db832f1a80e1bfb7
eyestep <eyestep@localhost>
2010-09-12 20:53:40 +0200

    Replace the reprevision script with a shell script again

---
+++ build/reprevision.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+#  This file is part of the heather package
+#
+#  Copyright (c) 2010 Gregor Klinke
+#  All rights reserved.
+#
+
+if [ -z "$HG" ]; then
+  HG=hg
+fi
+
+$HG id --debug | sed -e 's/\([a-zA-Z0-9]*\).*/\1/'
