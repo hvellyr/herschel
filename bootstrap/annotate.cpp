49e48a6524645c8ae2ff3c49821b3bdf449a394a
Gregor Klinke <hvellyr@users.noreply.github.com>
2011-01-16 23:06:51 +0100

    Improved wording in error message

--- bootstrap/annotate.cpp
+++ bootstrap/annotate.cpp
@@ -164,7 +164,7 @@ Annotator::annotate(SymbolNode* node)
 
     if (Properties::test_passLevel() > 2) {
       errorf(node->srcpos(), E_UndefinedVar,
-             "Unknown variable '%s' [1]", (const char*)StrHelper(node->name()));
+             "Unknown symbol '%s'", (const char*)StrHelper(node->name()));
       // node->scope()->dumpDebug(true);
     }
   }
