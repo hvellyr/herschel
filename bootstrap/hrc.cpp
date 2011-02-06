e2899556daf7e5934238eace595b0dacacc430ff
Gregor Klinke <hvellyr@users.noreply.github.com>
2011-02-06 19:59:03 +0100

    Added copyright header

--- bootstrap/hrc.cpp
+++ bootstrap/hrc.cpp
@@ -1,3 +1,16 @@
+/* -*-c++-*-
+
+   This file is part of the herschel package
+
+   Copyright (c) 2010-2011 Gregor Klinke
+   All rights reserved.
+*/
+
+//----------------------------------------------------------------------------
+
+using namespace herschel;
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
