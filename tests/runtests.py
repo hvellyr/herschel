7d084952c4a031fab92ce5b6f4b46b39de90d55f
Gregor Klinke <hvellyr@users.noreply.github.com>
2011-02-26 22:47:30 +0100

    Automatic run compile tests

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python2.5
 
+from __future__ import with_statement
+
 from optparse import OptionParser
 import xml.dom.minidom as minidom
 import subprocess
@@ -7,6 +9,11 @@ import os
 import sys
 import comparexml
 import re
+import tempfile
+import shutil
+import platform
+import threading
+import signal
 
 OPTIONS = { 'syntax':    { '1': ['-T', 'pass1',     '-P', '--dont-import', '--parse-1' ],
                            '2': ['-T', 'pass2',     '-P', '--dont-import', '--parse-2' ],
@@ -26,14 +33,71 @@ OPTIONS = { 'syntax':    { '1': ['-T', 'pass1',     '-P', '--dont-import', '--pa
 PHASES = ['1', '2', '3', '4', '5']
 
 
+
+#----------------------------------------------------------------------------
+
+class TimeoutException(Exception):
+    pass
+
+
+class DirSaver:
+    def __init__(self, new_dir=None):
+        self.new_dir = new_dir
+
+    def __enter__(self):
+        self.old_dir = os.getcwd()
+        if self.new_dir:
+            os.chdir(self.new_dir)
+
+    def __exit__(self, type, value, tb):
+        os.chdir(self.old_dir)
+
+
+class TmpDirCtx:
+    def __init__(self, prefix, delete_on_exit=True, base_dir=None):
+        self.prefix = prefix
+        self.delete_on_exit = delete_on_exit
+        self.base_dir = base_dir
+
+    def __enter__(self):
+        self.old_dir = os.getcwd()
+
+        if self.base_dir is not None:
+            self.tmp_dir = tempfile.mkdtemp(prefix=self.prefix, dir=self.base_dir)
+        else:
+            self.tmp_dir = tempfile.mkdtemp(prefix=self.prefix)
+
+        return self.tmp_dir
+
+    def __exit__(self, type, value, tb):
+        if self.delete_on_exit:
+            shutil.rmtree(self.tmp_dir)
+
+
+#----------------------------------------------------------------------------
+
+def kill_process(p, timed_out_mutable, process_name):
+    status = p.poll()
+    if status is None:
+        timed_out_mutable[0] = True
+
+        if platform.system() in ("Darwin", "Linux"):
+            os.killpg(p.pid, signal.SIGTERM)
+#        elif platform.system() == "Windows":
+#            call(['taskkill', '/PID', str(p.pid), '/T', '/F'], raise_on_error=False)
+
+
+#----------------------------------------------------------------------------
+
 class TestRunner:
 
     def __init__(self):
-        self.herschel_path = "../temp/debug/hrc"
+        self.executable_path = os.path.abspath("../temp/debug/hrc")
         self.verbose = False
         self.test_succeeded = 0
         self.test_run = 0
         self.input_dir = False
+        self.temp_dir = None
         self.last_passid = -1
         self.column_count = 0
         self.failures = []
@@ -50,9 +114,9 @@ class TestRunner:
         if self.verbose:
             print full_msg
         else:
-            self.break_cols()
             if self.last_passid >= 0:
                 sys.stdout.write('\b.')
+            self.break_cols()
             sys.stdout.write("E")
             self.column_count = self.column_count + 1
             self.failures.append(full_msg)
@@ -64,9 +128,9 @@ class TestRunner:
         if self.verbose:
             print full_msg
         else:
-            self.break_cols()
             if self.last_passid >= 0:
                 sys.stdout.write('\b.')
+            self.break_cols()
             sys.stdout.write("i")
             self.column_count = self.column_count + 1
             self.failures.append(full_msg)
@@ -77,10 +141,10 @@ class TestRunner:
         if self.verbose:
             print "OK: %s" % msg
         else:
-            self.break_cols()
             if self.last_passid >= 0:
                 sys.stdout.write("\b")
                 self.column_count = self.column_count - 1
+            self.break_cols()
             np = int(passid)
             sys.stdout.write(passid)
             self.column_count = self.column_count + len(passid)
@@ -94,13 +158,13 @@ class TestRunner:
 
     def close_report(self):
         if not self.verbose:
-            self.break_cols()
             if self.last_passid >= 0:
                 sys.stdout.write("\b.")
+            self.break_cols()
 
 
     def run_herschel_on_test(self, test_file, options):
-        cmd = [self.herschel_path]
+        cmd = [self.executable_path]
         cmd.extend(options)
         if self.input_dir:
             cmd.append('-I')
@@ -141,9 +205,9 @@ class TestRunner:
             return False
 
 
-    def find_expected_xml(self, test_file, passid):
-        path, ext = os.path.splitext(test_file)
-        expected_file = path + passid + ".xml"
+    def find_expected_file(self, test_file, passid, ext=".xml"):
+        path, srcext = os.path.splitext(test_file)
+        expected_file = path + passid + ext
 
         if os.path.exists(expected_file):
             return expected_file
@@ -154,7 +218,7 @@ class TestRunner:
 
     def run_pass_test_impl(self, test_file, options, passid,
                            errtest_func=None):
-        expected_file = self.find_expected_xml(test_file, "_%s" % (passid))
+        expected_file = self.find_expected_file(test_file, "_%s" % (passid))
         if not expected_file:
             return
 
@@ -172,7 +236,7 @@ class TestRunner:
                 return
 
 
-        expected_file = self.find_expected_xml(test_file, "_%s" % (passid))
+        expected_file = self.find_expected_file(test_file, "_%s" % (passid))
         if expected_file:
             test_count += 1
             if not self.compare_XML_result_with_file(what_tag, output, expected_file):
@@ -272,15 +336,156 @@ class TestRunner:
 
     #----------------------------------------------------------------------------
 
+    def os_ext(self):
+        # TODO: need platform
+        if platform.system() == "Windows":
+            return ".exe"
+        elif platform.system() == "Darwin":
+            return ""
+        elif platform.system() == "Linux":
+            return ""
+        else:
+            return ""
+
+
+    # returns [ Success, output, errout, returncode ]
+    def run_test_binary(self, test_file, options, timeout=20):
+        cmd = [test_file]
+        cmd.extend(options)
+
+        p = subprocess.Popen(cmd,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        timed_out_mutable = [False]
+
+        timer = threading.Timer(float(timeout), kill_process,
+                                [p, timed_out_mutable, cmd[0] ])
+        timer.start()
+
+        timed_out = False
+        try:
+            output, erroutput = p.communicate()
+        finally:
+            timed_out = timed_out_mutable[0]
+            timer.cancel()
+
+        if timed_out:
+            raise TimeoutException(test_file)
+
+        return p.returncode, output, erroutput 
+
+
+    def load_testdesc(self, test_file):
+        compiledesc_file = self.find_expected_file(test_file, "", ext=".testdesc")
+        if not compiledesc_file:
+            return None
+
+        try:
+            f = file(compiledesc_file)
+            desc = eval(f.read())
+            return desc;
+        finally:
+            if f is not None:
+                f.close()
+        return None
+
+
+    def check_contains_patterns(self, text, patterns):
+        for p in patterns:
+            repattern = re.compile(p)
+            if repattern.search(text) is None:
+                return False
+        # all patterns match
+        return True
+
+
+    def check_tmp_run_simple(self, passid, what_tag, retc, binout, binerr):
+        if retc != 0:
+            self.report_info("%s: %d" % (what_tag, retc))
+            self.report_failure("%s: test failed with returncode != 0" % what_tag)
+        else:
+            self.report_success(passid, "%s: test succeeds" % what_tag)
+            self.test_succeeded += 1
+
+
+    def check_tmp_run_detailed(self, passid, what_tag, retc, binout, binerr, compiledesc):
+        if "retc" in compiledesc:
+            expected_retc = compiledesc["retc"]
+        else:
+            expected_retc = 0
+
+        if retc != expected_retc:
+            self.report_info("%s: %d" % (what_tag, retc))
+            self.report_failure("%s: test failed with returncode != 0" % what_tag)
+            return
+
+        if compiledesc.has_key("output"):
+            if not self.check_contains_patterns(binout, compiledesc["output"]):
+                print "BINOUT: <%s>" % binout
+                print "EXPOUT: <%s>" % compiledesc["output"]
+                self.report_failure("%s: differs from expected output" % what_tag)
+                return
+
+        self.report_success(passid, "%s: test succeeds" % what_tag)
+        self.test_succeeded += 1
+
+
+    def run_test_binary_impl(self, tmp_binary, compiledesc, passid, what_tag):
+        try:
+            retc, binout, binerr = self.run_test_binary(tmp_binary, [])
+
+            if compiledesc is None:
+                self.check_tmp_run_simple(passid, what_tag, retc, binout, binerr)
+            else:
+                self.check_tmp_run_detailed(passid, what_tag,
+                                            retc, binout, binerr, compiledesc)
+
+        except TimeoutException:
+            self.report_failure("%s: test timed out" % what_tag)
+
+        self.test_run += 1
+
+
+    def run_compile_test(self, test_file, options, passid, errtest_func=None):
+        compiledesc = self.load_testdesc(os.path.abspath(test_file))
+
+        with TmpDirCtx("test_", base_dir=self.temp_dir, delete_on_exit=True) as tmp_dir:
+            abs_test_file = os.path.abspath(test_file)
+
+            with DirSaver(tmp_dir):
+                basename, ext = os.path.splitext(os.path.basename(test_file))
+                tmp_binary = os.path.abspath(basename + self.os_ext())
+
+                options = [ "-o", tmp_binary ]
+                output, erroutput = self.run_herschel_on_test(abs_test_file, options)
+                what_tag = "[%s] %s" % (passid, test_file)
+                if erroutput:
+                    self.report_failure("%s: %s" % (what_tag, erroutput))
+                    self.test_run += 1
+                    return
+
+#                if output:
+#                    print "Output: %s" % output
+
+                self.run_test_binary_impl(tmp_binary, compiledesc, passid, what_tag)
+
+            return
+
+
+    #----------------------------------------------------------------------------
+
     def run_test(self, test_dir, src_file, domain):
         test_file = os.path.join(test_dir, src_file)
 
-        if src_file.startswith("failed-"):
-            self.run_pass_failed_test(test_file, domain)
-        elif src_file.startswith("ignore-"):
-            pass
+        if domain == "compile":
+            self.run_compile_test(test_file, "", "6")
         else:
-            self.run_pass_test(test_file, domain)
+            if src_file.startswith("failed-"):
+                self.run_pass_failed_test(test_file, domain)
+            elif src_file.startswith("ignore-"):
+                pass
+            else:
+                self.run_pass_test(test_file, domain)
 
 
     def run_all_tests(self, test_dir, domain):
@@ -324,19 +529,24 @@ def main():
     parser.add_option("-I", "--input",
                       dest="input", default=".",
                       help="give the input directory to the compiler")
-
+    parser.add_option("-t", "--temp-dir",
+                      dest="temp_dir", default=None,
+                      help="give a directory where temporary data can be stored")
     (options, args) = parser.parse_args()
 
     tr = TestRunner()
 
     if options.executable is not None:
-        tr.herschel_path = options.executable
+        tr.executable_path = os.path.abspath(options.executable)
 
     if options.verbose:
         tr.verbose = options.verbose
 
     if options.input:
         tr.input_dir = options.input
+
+    if options.temp_dir:
+        tr.temp_dir = options.temp_dir
 
     for arg in args:
         if os.path.isdir(arg):
