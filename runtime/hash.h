64ff6aa95597faaa68015c4749dad84c3e46487a
Gregor Klinke <hvellyr@users.noreply.github.com>
2011-05-31 19:27:43 +0200

    Fix stupid hash implementation
    This fixes the failing test with llvm-2.9...

--- runtime/hash.h
+++ runtime/hash.h
@@ -23,20 +23,33 @@ struct HashNode
 };
 
 
+typedef unsigned int (*hashtable_hash_func)(void* key);
+typedef int (*hashtable_cmp_func)(void* one, void* two);
+
 typedef struct HashTable HashTable;
 struct HashTable
 {
   HashNode**  fNodes;
   size_t      fSize;
   size_t      fItems;
+  hashtable_hash_func fHashFunc;
+  hashtable_cmp_func  fKeyCmp;
 };
 
 
-HashTable* hashtable_alloc(size_t size);
+HashTable* hashtable_alloc(size_t size,
+                           hashtable_hash_func hashFunc,
+                           hashtable_cmp_func cmpFunc);
 void hashtable_free(HashTable* table);
 void hashtable_add(HashTable* table, void* key, void* value);
 void hashtable_add_all(HashTable* table, HashTable* other);
 void* hashtable_get(HashTable* table, void* key);
 void hashtable_remove(HashTable* table, void* key);
+
+unsigned int hashtable_sizet_func(void* key);
+int hashtable_sizet_cmp_func(void* one, void* two);
+
+unsigned int hashtable_cstr_func(void* key);
+int hashtable_cstr_cmp_func(void* one, void* two);
 
 #endif                          // runtime_hash_h
