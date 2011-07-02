6d3ab04e03a5966f0b3f611ba1fa2155af9a0024
Gregor Klinke <hvellyr@users.noreply.github.com>
2011-07-03 01:28:45 +0200

    Add helper functions to simplify debugging in gdb

---
+++ bootstrap/gdb.cpp
@@ -0,0 +1,27 @@
+/* -*-c++-*-
+
+   This file is part of the herschel package
+
+   Copyright (c) 2010-2011 Gregor Klinke
+   All rights reserved.
+
+   This source code is released under the BSD License.
+*/
+
+#include <stdio.h>
+
+#include "common.h"
+#include "str.h"
+
+#if defined(IS_DEBUG)
+
+extern "C"
+void gdb_ps(herschel::String str);
+
+
+void gdb_ps(herschel::String str)
+{
+  fprintf(stderr, "%s\n", (const char*)herschel::StrHelper(str));
+}
+#endif
+
