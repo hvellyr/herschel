dc4b367f8c92de4e559416fd298d644276ec4dda
eyestep <eyestep@localhost>
2012-04-06 11:30:17 +0200

    Base implementation of lang|append for lang|Strings

--- runtime/rtstr.c
+++ runtime/rtstr.c
@@ -27,7 +27,7 @@
 typedef unsigned short Ucs2Char;
 
 
-void allocate_string(ATOM* instance, const char* str)
+void allocate_string(struct ATOM* instance, const char* str)
 {
   size_t items = 0;
 
@@ -41,6 +41,7 @@ void allocate_string(ATOM* instance, const char* str)
   instance->u.v_obj = malloc(sizeof(size_t) + items * sizeof(Ucs2Char));
 
   *((size_t*)instance->u.v_obj) = items;
+  if (items > 0)
   {
     Ucs2Char* p = (Ucs2Char*)(instance->u.v_obj + sizeof(size_t));
     memset(p, 0, items * sizeof(Ucs2Char));
@@ -49,6 +50,36 @@ void allocate_string(ATOM* instance, const char* str)
     for (i = 0; i < items; i++, p++)
       *p = str[i];
   }
+}
+
+
+struct ATOM allocate_string_n(size_t items, unsigned int c)
+{
+  struct ATOM instance;
+
+#if defined(UNITTESTS)
+  hr_trace("allocate",
+           "Create new instance of type 'lang|String' with length %ld: '%d'",
+           items, c);
+#endif
+
+//  fprintf(stderr, "%p\n", instance);
+
+  instance.typeid = TYPE_TAG_STRINGIMPL;
+  instance.u.v_obj = malloc(sizeof(size_t) + items * sizeof(Ucs2Char));
+
+  *((size_t*)instance.u.v_obj) = items;
+  if (items > 0)
+  {
+    Ucs2Char* p = (Ucs2Char*)(instance.u.v_obj + sizeof(size_t));
+    memset(p, 0, items * sizeof(Ucs2Char));
+
+    size_t i;
+    for (i = 0; i < items; i++, p++)
+      *p = c;
+  }
+
+  return instance;
 }
 
 
@@ -104,4 +135,16 @@ unsigned int lang_string_char_at_index(struct ATOM atom0,
   }
 
   return 0;
+}
+
+void lang_string_set_char_at_index(struct ATOM atom0,
+                                   unsigned int idx, unsigned int c)
+{
+  if (atom0.typeid == TYPE_TAG_STRINGIMPL) {
+    size_t len = *((size_t*)atom0.u.v_obj);
+    if (idx < len) {
+      Ucs2Char* p0 = (Ucs2Char*)(atom0.u.v_obj + sizeof(size_t));
+      p0[idx] = c;
+    }
+  }
 }
