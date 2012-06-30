09fc420f4a8840662ea13d1aa4d6c5296a802cf9
Gregor Klinke <hvellyr@users.noreply.github.com>
2012-07-01 00:19:00 +0900

    Updated copyright mark

--- doc/copyright.c
+++ doc/copyright.c
@@ -2,7 +2,7 @@
 
    This file is part of the herschel package
 
-   Copyright (c) 2010-2011 Gregor Klinke
+   Copyright (c) 2010-2012 Gregor Klinke
    All rights reserved.
 
    This source code is released under the BSD License.
