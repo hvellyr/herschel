c7e419c83ec4b472f0b63f8edf79fb5162396f94
hvellyr <gck@eyestep.org>
2020-05-24 21:54:08 +0200

    Reduce size of SrcPos by only storing index to cache file table

--- bootstrap/srcpos.cpp
+++ bootstrap/srcpos.cpp
@@ -13,12 +13,65 @@
 #include "str.hpp"
 #include "strbuf.hpp"
 
+#include <unordered_map>
+
 
 namespace herschel {
 
+namespace {
+  std::unordered_map<String, int>& path2IdxCache()
+  {
+    static auto cache = std::unordered_map<String, int>{};
+    return cache;
+  }
+
+  std::unordered_map<int, String>& idx2PathCache()
+  {
+    static auto cache = std::unordered_map<int, String>{};
+    return cache;
+  }
+
+  int pathToIndex(const String& file)
+  {
+    static auto nextFileIndex = 0;
+
+    auto iFile = path2IdxCache().find(file);
+    if (iFile != end(path2IdxCache())) {
+      return iFile->second;
+    }
+
+    auto idx = ++nextFileIndex;
+    path2IdxCache()[file] = idx;
+    idx2PathCache()[idx] = file;
+
+    return idx;
+  }
+
+  const String& indexToPath(int idx)
+  {
+    static auto nullValue = String{};
+    auto iIdx = idx2PathCache().find(idx);
+    return iIdx != end(idx2PathCache()) ? iIdx->second : nullValue;
+  }
+}  // namespace
+
+
+SrcPos::SrcPos(const String& file, int lineno, int colno)
+    : fFileIdx(pathToIndex(file))
+    , fLineNo(lineno)
+    , fColNo(colno)
+{
+}
+
+
+const String& SrcPos::file() const
+{
+  return indexToPath(fFileIdx);
+}
+
 String SrcPos::toString() const
 {
-  return (StringBuffer(fFile) << ':' << fromInt(fLineNo) << ':' << fromInt(fColNo))
+  return (StringBuffer(file()) << ':' << fromInt(fLineNo) << ':' << fromInt(fColNo))
       .toString();
 }
 
--- bootstrap/srcpos.hpp
+++ bootstrap/srcpos.hpp
@@ -22,57 +22,34 @@ namespace herschel {
 class SrcPos {
 public:
   //! Constructs a default source position, pointing nowhere.
-  SrcPos()
-      : fLineNo(0)
-      , fColNo(0)
-  {
-  }
+  SrcPos() = default;
 
   //! Constructs a source position refering to \p file and line number \p
   //! lineno.
-  SrcPos(const String& file, int lineno, int colno)
-      : fFile(file)
-      , fLineNo(lineno)
-      , fColNo(colno)
-  {
-  }
+  SrcPos(const String& file, int lineno, int colno);
 
   //! Constructs a source position refering to \p file and line number \p
   //! lineno.
   SrcPos(zstring file, int lineno, int colno)
-      : fFile(String(file))
-      , fLineNo(lineno)
-      , fColNo(colno)
+    : SrcPos(String(file), lineno, colno)
   {
-  }
-
-  //! Copy constructor
-  SrcPos(const SrcPos& other) { *this = other; }
-
-  //! Assign operator
-  SrcPos& operator=(const SrcPos& other)
-  {
-    fFile = other.fFile;
-    fLineNo = other.fLineNo;
-    fColNo = other.fColNo;
-    return *this;
   }
 
   //! Indicates whether *this is a valid source position, i.e. whether it
   //! refers to a file and lineno.
-  bool isValid() const { return !fFile.isEmpty() && fLineNo != 0; }
+  bool isValid() const { return fFileIdx >= 0 && fLineNo != 0; }
 
   //! Compare operator
   bool operator==(const SrcPos& other) const
   {
-    return fFile == other.fFile && fLineNo == other.fLineNo && fColNo == other.fColNo;
+    return fFileIdx == other.fFileIdx && fLineNo == other.fLineNo && fColNo == other.fColNo;
   }
 
   //! Compare operator
   bool operator!=(const SrcPos& other) const { return !(*this == other); }
 
   //! Returns the file name portion of *this.
-  const String& file() const { return fFile; }
+  const String& file() const;
 
   //! Returns the line number portion of *this.
   int lineNumber() const { return fLineNo; }
@@ -85,9 +62,9 @@ public:
   String toString() const;
 
 private:
-  String fFile;
-  int fLineNo;
-  int fColNo;
+  int fFileIdx = -1;
+  int fLineNo = 0;
+  int fColNo = 0;
 };
 
 }  // namespace herschel
